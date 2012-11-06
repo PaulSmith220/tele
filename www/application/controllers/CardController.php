55a003339e373cfc0fb043ea0e5d790f9794ad1c
Paul Smith <kuznetps@gmail.com>
2012-11-06 15:55:33 +0600

    Модель для работы с таблицей objectively

--- www/application/controllers/CardController.php
+++ www/application/controllers/CardController.php
@@ -32,9 +32,13 @@ class CardController extends Zend_Controller_Action
      
        $mforms = new Model_forms();
        $forms = array();
+       $objs = array();
+       $neus = array();
        $fset = $mforms->fetchAll('id='.$post);
        foreach ($fset as $f){
           $forms = explode(',',$f->complaints);
+          $objs = explode(',',$f->objectively);
+          $neus = explode(',',$f->neurostatus);
        }
        $this->view->content = '';
        
@@ -47,12 +51,12 @@ class CardController extends Zend_Controller_Action
            foreach($cset as $c){
              
               
-                $this->view->content .= "<a href=# class='ln'><b>".$c->name. "</b></a>";
+                $this->view->content .= "<a  class='ln'><b>".$c->name. "</b></a>";
               $this->view->content .= "<ul style='display:none;'>";
               $items = explode("&",$c->items);
               foreach($items as $it){
                       $this->view->content .= "<li>";
-                   $this->view->content .= "<a href=#  class='0' title='".$c->contents."'>".$it."</a>";
+                   $this->view->content .= "<a  class='0' title='".$c->contents."'>".$it."</a>";
                     $this->view->content .= "</li>";
               }
              
@@ -64,12 +68,75 @@ class CardController extends Zend_Controller_Action
      
        //Далее выводи анамнез
        
-         $this->view->content .= "</ul><textarea style='display:none' id='anamnesis' placeholder='Анамнез в свободной форме'></textarea>"; 
+         $this->view->content .= "</ul><textarea style='display:none' id='anamnesis' placeholder='Анамнез в свободной форме'>
+1. Пол:
+2. Возраст: 
+3. Причина обращения:</textarea>"; 
        
       // Объективно:
+         $mobj = new Model_Obj(); 
+         foreach ($objs as $obj){
+          $objset = $mobj->fetchAll("id='".$obj."'");         
+            foreach($objset as $o){
+                 $this->view->content .= "<ul id='obj_ul' style='display:none;'><li>";
+                //Разбиваем список разделов:
+                $o_razd = array();
+                $o_razd = explode(',',$o->fields);
+                //Разбиваем список элементов на разделы
+                $o_razd_i = array();
+                $o_razd_i = explode('|',$o->items);
+                $i=-1;
+                //Выводим каждый раздел:
+                    foreach($o_razd as $r){
+                      $i++;  
+                      
+               $this->view->content.= "<a  class='ln'><b>".$r."</a>";
+               
+                //Разбиваем раздел на элементы
+                $rr = array();
+                $rr = explode("&",$o_razd_i[$i]);
+                //Выводим содержание раздела.
+                $this->view->content.="<ul>";
+                foreach($rr as $item){
+                    $this->view->content.= "<li>";
+                    $this->view->content.="<a   class='0' title='".$r."'>".$item."</a>";
+                    $this->view->content.="</li>";
+               }
+                $this->view->content.="</ul>";
+                      
+                    }
+                
+                
+                $this->view->content .= "</li></ul>";
+            }
+          
+         }
+         $m_new = new Model_Neuro();
+         foreach($neus as $n){
+             $nset = $m_new->fetchAll("id='".$n."'");
+             foreach($nset as $new){
+                    $this->view->content .= "<ul id='neuro' style='display:none;'><li>";
+                 $n_zag = array();
+                 $n_fields = array();
+                 $n_zag = explode(',',$new->fields);
+                 $n_fields = explode('|',$new->items);
+                 $i = -1;
+                 foreach ($n_zag as $zag){
+                     $i++;
+                     $n_fields_i = explode("&",$n_fields[$i]);
+                     $this->view->content.= "<a href=# class='ln'><b>".$zag."</a>";
+                     $this->view->content.="<ul style='display:none;'>";
+                     foreach($n_fields_i as $item){
+                             $this->view->content.= "<li>";
+                         $this->view->content.="<a   class='0' title='".$zag."'>".$item."</a>";
+                             $this->view->content.= "</li>";
+                     }
+                     $this->view->content.="</ul>";        
+                 }
+             }
+             $this->view->content.="</li></ul>";
+         }
          
-                   
-      
        
       
     }else  { if(isset($_SESSION['login'])) unset($_SESSION['login']);
---
+++ www/application/models/neuro.php
@@ -0,0 +1,10 @@
+<?php
+class Model_Neuro extends Zend_Db_Table_Abstract {
+  protected $_name = 'neurostatus';
+  
+  public function readNeuro($data){
+ 
+ 
+}
+  
+}
